12a90ad3c4a588f950677e1f3597218339000a7e
Soumyadeep Bhowmik <166634787+Soumyadeep2124@users.noreply.github.com>
2025-07-29 17:19:47 +0530

    Update axiosClient.js

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://repointer.in',
+    baseURL: 'https://www.repointer.in',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
